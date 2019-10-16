b78c9835491213e22f682a56c99a2ea863aa8707
datsuns <the.skeleton7@gmail.com>
2019-10-16 11:16:48 +0900

    fix space

--- lib/main.dart
+++ lib/main.dart
@@ -67,15 +67,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
 
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Text(
-            'You have pushed the button this many times:',
-          ),
           Text(
             '$_counter',
             style: Theme.of(context).textTheme.display1,
           ),
+          generateOneLine(),
+          generateOneLine(),
           generateOneLine(),
           generateOneLine(),
         ],
